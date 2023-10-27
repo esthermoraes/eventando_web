cad00531d5a62fb2954cf761bb2190318457e82f
esthermoraes <esther.ifes2021@gmail.com>
2023-10-26 22:10:19 -0300

    aaaaaaa

--- criarEventoP.php
+++ criarEventoP.php
@@ -1,23 +1,23 @@
 <!-- Aqui iniciamos o código html -->
 <html>
 <head>
-	<meta charset = "utf-8">
-	<meta name = "viewport" content = "width=device-width, initial-scale = 1, maximum-scale = 1">
-	<!-- Icons -->
-	<link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
-	<!-- Bootstrap -->
-	<link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin = "anonymous">
-	<script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin = "anonymous"></script>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
-	<!-- CSS externo -->
-	<link rel="stylesheet" href="css/css_header.css">
-	<link rel = "stylesheet" type = "text/css" href = "css/css_criarEventoP.css" />
+    <meta charset = "utf-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1, maximum-scale = 1">
+    <!-- Icons -->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+    <!-- Bootstrap -->
+    <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel = "stylesheet" integrity = "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin = "anonymous">
+    <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin = "anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <!-- CSS externo -->
+    <link rel="stylesheet" href="css/css_header.css">
+    <link rel = "stylesheet" type = "text/css" href = "css/css_criarEventoP.css" />
     <!-- JS externo-->
     <script src="js/js_criarEventoP.js" defer></script>
-	<!-- Definimos o título da página -->
-	<title> CRIAR EVENTO PRESENCIAL </title>
+    <!-- Definimos o título da página -->
+    <title> CRIAR EVENTO PRESENCIAL </title>
     <!-- Definimos o ícone na aba da página-->
-	<link rel="shortcut icon" type="image/png" href="img/calendar_icon.png"/>
+    <link rel="shortcut icon" type="image/png" href="img/calendar_icon.png"/>
 </head>
 
 <body>
@@ -46,10 +46,10 @@
         </div>
     </header>
 
-	<div class="container-fluid mt-5 ms-0 d-flex justify-content-between titulo">
-		<p class="ms-3">INFORMAÇÕES DO EVENTO</p>
+    <div class="container-fluid mt-5 ms-0 d-flex justify-content-between titulo">
+        <p class="ms-3">INFORMAÇÕES DO EVENTO</p>
         <p class="me-3">Nome do Evento</p>
-	</div>
+    </div>
     <div class="container-fluid d-flex p-0 bagulhete">
         <div class="div_passos">
             <div id="btn-passo1" class="passo1 mb-2">
@@ -66,17 +66,17 @@
             <div class="div-form">
                 <form class="d-flex flex-wrap">
                     <div class="div-img d-flex flex-wrap w-100">
-                            <div>
-                                <label class="imagem" for="file">FOTO DO EVENTO</label>
-                                <input id="file" type="file"/>
+                        <div class="col-5">
+                            <label class="imagem" for="file">FOTO DO EVENTO</label>
+                            <input id="file" type="file"/>
+                        </div>
+                        <div class="col-5 ms-5 w-50">
+                            <input placeholder="Objetivo do evento" class="obj form-control"/>
+                            <div class="d-flex justify-content-between mt-5">
+                                <input class="form-control me-3" type="text" id="date" placeholder="Data Prevista" onfocus="(this.type='date')" onblur="(this.type='text')">
+                                <input class="form-control horario" type="text" id="time" placeholder="Horário" onfocus="(this.type='time')" onblur="(this.type='text')"/>
                             </div>
-                            <div class="infos-basicas">
-                                <input placeholder="Objetivo do evento" class="obj form-control"/>
-                                <div class="d-flex justify-content-between mt-5">
-                                    <input class="form-control me-3" type="text" id="date" placeholder="Data Prevista" onfocus="(this.type='date')" onblur="(this.type='text')">
-                                    <input class="form-control horario" type="text" id="time" placeholder="Horário" onfocus="(this.type='time')" onblur="(this.type='text')"/>
-                                </div>
-                            </div>
+                        </div>
                             <!-- <div class="d-flex align-items-center datetime"></div> -->
                     </div>
                     <div class="lado1">
@@ -86,33 +86,33 @@
                                 <input id="cep" class="form-control" placeholder="CEP"/>
                                 <select class="form-select uf me-5" id="sltEstado">
                                     <option value="">Estado</option>
-                                    <option value="AC">Acre</option>
-                                    <option value="AL">Alagoas</option>
-                                    <option value="AP">Amapá</option>
-                                    <option value="AM">Amazonas</option>
-                                    <option value="BA">Bahia</option>
-                                    <option value="CE">Ceará</option>
-                                    <option value="DF">Distrito Federal</option>
-                                    <option value="ES">Espirito Santo</option>
-                                    <option value="GO">Goiás</option>
-                                    <option value="MA">Maranhão</option>
-                                    <option value="MS">Mato Grosso do Sul</option>
-                                    <option value="MT">Mato Grosso</option>
-                                    <option value="MG">Minas Gerais</option>
-                                    <option value="PA">Pará</option>
-                                    <option value="PB">Paraíba</option>
-                                    <option value="PR">Paraná</option>
-                                    <option value="PE">Pernambuco</option>
-                                    <option value="PI">Piauí</option>
-                                    <option value="RJ">Rio de Janeiro</option>
-                                    <option value="RN">Rio Grande do Norte</option>
-                                    <option value="RS">Rio Grande do Sul</option>
-                                    <option value="RO">Rondônia</option>
-                                    <option value="RR">Roraima</option>
-                                    <option value="SC">Santa Catarina</option>
-                                    <option value="SP">São Paulo</option>
-                                    <option value="SE">Sergipe</option>
-                                    <option value="TO">Tocantins</option>
+                                    <option value="1">Acre</option>
+                                    <option value="2">Alagoas</option>
+                                    <option value="3">Amapá</option>
+                                    <option value="4">Amazonas</option>
+                                    <option value="5">Bahia</option>
+                                    <option value="6">Ceará</option>
+                                    <option value="7">Distrito Federal</option>
+                                    <option value="8">Espirito Santo</option>
+                                    <option value="9">Goiás</option>
+                                    <option value="10">Maranhão</option>
+                                    <option value="11">Mato Grosso do Sul</option>
+                                    <option value="12">Mato Grosso</option>
+                                    <option value="13">Minas Gerais</option>
+                                    <option value="14">Pará</option>
+                                    <option value="15">Paraíba</option>
+                                    <option value="16">Paraná</option>
+                                    <option value="17">Pernambuco</option>
+                                    <option value="18">Piauí</option>
+                                    <option value="19">Rio de Janeiro</option>
+                                    <option value="20">Rio Grande do Norte</option>
+                                    <option value="21">Rio Grande do Sul</option>
+                                    <option value="22">Rondônia</option>
+                                    <option value="23">Roraima</option>
+                                    <option value="24">Santa Catarina</option>
+                                    <option value="25">São Paulo</option>
+                                    <option value="26">Sergipe</option>
+                                    <option value="27">Tocantins</option>
                                 </select>
                             </div>
                             <div class="d-flex flex-column pe-5">
@@ -126,7 +126,7 @@
                         </div>
                     </div>
                     <div class="ms-5 lado2">                
-                        <div class = "">
+                        <div class = "col- w-100">
                             <label class="mb-3 complemento" for="buffet">Complementos:</label>
                             <div class="mb-3 d-flex justify-content-between">
                                 <textarea id = "buffet" placeholder="Buffet" class="form-control buffet"></textarea>
@@ -146,12 +146,13 @@
                                 <i class="mt-2 fa-solid fa-unlock fa-flip-horizontal fa-xl" style="color: #b25abf;"></i>
                                 <p class="ms-2 pp">Público</p>
                             </div>
+                            <a href="passo2.php" class="align-items-end">
+                                <button type="submit" id="btn-passo2" class="botao">&#10140; PRÓXIMO PASSO</button>
+                            </a>
                         </div>
                     </div>
-                    <div class="b">
-                        <a href = "passo2.php">
-                            <button type="submit" id="btn-passo2" class="botao">&#10140; PRÓXIMO PASSO</button>
-                        </a>
+                    <div class="">
+                        
                     </div>
                 </form>
             </div>
