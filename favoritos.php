cf55e52a784e9fcdb122f669cc43db22eaf56267
esthermoraes <esther.ifes2021@gmail.com>
2023-11-19 14:27:33 -0300

    favoritos ok

--- favoritos.php
+++ favoritos.php
@@ -11,20 +11,18 @@
     include_once 'header.php';
 ?>
 
-	
 	<div class="mae">
 		<div class="row">
-			<div class="col-5 ms-5 mt-5 py-5">
+			<div class="col-5 ms-5 mtt">
 				<h1 class="titulo">
 					<i class="fa-solid fa-star me-2" style="color: #f5d742;"></i>
 					FAVORITOS
 				</h1>
 			</div>
 
-			<div class="col-5 ms-5 mt-5 py-4 d-block d-sm-flex d-md-flex justify-content-end">
-				<!-- <i class="fa-solid fa-filter filtro mt-3 me-3 d-none d-sm-flex" style="color: #770089;"></i> -->
-				<select class="form-select mt-3" id="sltFiltros">
-					<option value="">&#xf0b0; Todos Eventos</option>
+			<div class="col-5 ms-5 me-0 mt-5 d-flex justify-content-end">
+				<select class="form-select mt-4 ms-5 me-0" id="sltFiltros">
+					<option value="TE">&#xf0b0; Todos Eventos</option>
 					<option value="ME">&#xf274; Meus Eventos</option>
 					<option value="ER">&#xf073; Eventos Recentes</option>
 					<option value="EP">&#xf3c5; Eventos Presenciais</option>
@@ -36,12 +34,13 @@
 
         <div class="ms-5 me-5 mb-1 teste">
             <div class="ms-2 me-2 mt-5 mb-5 gx-5">
-				<div class="row m-2 justify-content-center">
+				<div class="row m-2 justify-content-between">
+
 					<div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
 						<img src="img/evento1.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
-							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<i class="fa-solid fa-star ms-0 mt-0 me-2 fs-5" style="color: #f5d742;"></i>
+							<p class="card-title titulo-card ms-0 mt-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -49,7 +48,7 @@
 						<img src="img/evento2.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -57,7 +56,7 @@
 						<img src="img/evento3.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -65,7 +64,7 @@
 						<img src="img/evento4.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -73,7 +72,7 @@
 						<img src="img/evento5.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -81,7 +80,7 @@
 						<img src="img/evento6.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -89,7 +88,7 @@
 						<img src="img/evento7.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -97,7 +96,7 @@
 						<img src="img/evento8.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -105,7 +104,7 @@
 						<img src="img/evento9.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -113,7 +112,7 @@
 						<img src="img/evento10.jpg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -121,7 +120,7 @@
 						<img src="img/evento11.jpg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -129,7 +128,7 @@
 						<img src="img/evento12.jfif" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -137,7 +136,7 @@
 						<img src="img/evento13.jfif" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -145,7 +144,7 @@
 						<img src="img/evento14.jfif" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -153,23 +152,23 @@
 						<img src="img/evento15.jfif" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
-					<div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
+					<!-- <div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
 						<img src="img/evento16.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
-					</div>
+					</div> -->
 
 					<div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
 						<img src="img/evento17.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -177,7 +176,7 @@
 						<img src="img/evento18.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -185,15 +184,15 @@
 						<img src="img/evento19.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
-					<div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
+					<!-- <div class="card bg-transparent config-card mt-4 m-4 mb-4 p-0">
 						<img src="img/evento20.jfif" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
 					</div>
 
@@ -201,9 +200,10 @@
 						<img src="img/evento21.jpeg" class="card-img-top config-img" alt="foto evento">
 						<div class="card-img-overlay d-flex">
 							<i class="fa-solid fa-star ms-0 mt-2 me-2 fs-5" style="color: #f5d742;"></i>
-							<p class="card-title titulo-card fs-3 ms-0 fs-4">Título</p>
+							<p class="card-title titulo-card ms-0 fs-4">Título</p>
 						</div>
-					</div>
+					</div> -->
+
 				</div>
         	</div>
     	</div>
